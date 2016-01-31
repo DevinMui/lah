5eee64aac4d4ae9a61a99453e809c09763de83a7
Jesse Liang <jesseliang@ymail.com>
2016-01-30 17:58:26 -0800

    chrome things

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,1 +1,13 @@
-alert("Hello from your Chrome extension!")
+//var xhr = new XMLHttpRequest();
+//xhr.open("GET", "http://4b4084d9.ngrok.io", true);
+//xhr.send();
+
+//console.log(xhr.status);
+//console.log(xhr.statusText);
+
+function windowLoaded() {
+    
+    var answers = document.getElementById('answers').innerHTML;
+   	document.write(answers)
+};
+
