6ba9bc22d2f2788687beec1e519a275c27a7294b
Jesse Liang <jesseliang@ymail.com>
2016-01-31 00:41:41 -0800

    fixed fucking bugs and killed the shit out of them until they got squished to fucking death

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -5,31 +5,45 @@ $(document).ready(function() {
 	// gets all links on the page
 	$('a').each(function(){
 		try { // anything in the following block generates an error, ignore it and console log the error instead of killing the program
-			var answer_id = $('a').attr('name'); // gets the id from the link
-			var xhr = new XMLHttpRequest(); // makes new api request to my server on my laptop
-			var params = JSON.stringify({"answer_id": answer_id}) // parameters to send to my server
-			xhr.open("POST", "http://4b4084d9.ngrok.io/links/find_post", true) // starts post to server
-			xhr.setRequestHeader("Accept", "application/json");
-			xhr.onreadystatechange = function () { // checks if async task is completed and returns data
-		        if (xhr.readyState == 4) {
-		            if (xhr.status == 200) {
-		                var data = xhr.responseText;
-		                console.log(data)
-		            }
-		        }
-		    };
-			xhr.send(params) // sends parameters to server
+			var answer_id = $(this).attr('name'); // gets the id from the link
+			//console.log(answer_id)
+			if(answer_id == undefined)
+				return true
+			var string = "#answer-" + answer_id
+			for(var i=0;i<$(string).find(".post-text").find("a").length;i++){
+				//var xhr = new XMLHttpRequest(); // makes new api request to my server on my laptop
+				var params = {answer_id: answer_id, number: i} // parameters to send to my server
+				console.log(params)
+				/*xhr.open("POST", "http://4b4084d9.ngrok.io/links/find_post", true) // starts post to server
+				xhr.setRequestHeader("Accept", "application/json");
+				xhr.onreadystatechange = function () { // checks if async task is completed and returns data
+			        if (xhr.readyState == 4) {
+			           if (xhr.status == 200) {
+			                var data = xhr.responseText;
+			                console.log(data)
+			            }
+			        }
+			    };
+				xhr.send(params) // sends parameters to server*/
+				$.ajax({
+				    url: 'http://4b4084d9.ngrok.io/links/find_post',
+				    headers: {
+				    	"Accept": "application/json"
+				    },
+				    type: 'POST',
+				    data: JSON.stringify(params),
+				    contentType: 'application/json; charset=utf-8',
+
+				    dataType: 'json',
+				    async: true,
+				    success: function(msg) {
+				        console.log(msg);
+				    }
+				});
+			}
 		} catch(err) {
 			console.log("this a tag has no name attribute");
 		}
 	})
 	
 });
-
-//for (i = 0, i < 10, i++) {
-	//var xhr = new XMLHttpRequest();
-	//xhr.open("POST", "http://4b4084d9.ngrok.io/links/find_post", true);
-	//xhr.send();
-//};
-console.log(xhr.status);
-console.log(xhr.statusText);
